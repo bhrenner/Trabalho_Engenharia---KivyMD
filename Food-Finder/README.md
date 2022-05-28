6520a0fc910f111490a4f519785c36f35872878a
Bhrenner Wilson <89161865+bhrenner@users.noreply.github.com>
2022-05-28 14:52:04 -0300

    Pasta com a API funcionando, splash
    Falta acrescentar meus códigos e arrumar o background

---
+++ Food-Finder/README.md
@@ -0,0 +1,7 @@
+# Food-Finder
+Busca restaurantes mais próximos do usuário, baseado no tipo de comida que este deseja comer.
+
+# Como usar
+O usuário precisa inserir o tipo de comida que deseja comer e o local aonde se encontra, pode ser nome da rua, avenida, bairro ou até mesmo o nome específico do local.
+
+O programa retorna o nome do restaurante, endereço e foto do local.
---
+++ Food-Finder/app/main.py
@@ -0,0 +1,73 @@
+from kivymd.app import MDApp
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivymd.uix.card import MDCard
+from kivy.core.window import Window
+from kivy.clock import Clock
+import finder 
+
+Window.size = (400,750)
+
+class Splash(Screen):
+    def login(self):
+        MDApp.get_running_app().root.current = "tela_login"
+    
+    Clock.schedule_once(login, 5)
+
+
+class ResultadosCard(MDCard):
+   def __init__(self,name='',address='',photo='',**kwargs):
+       super().__init__(**kwargs)
+       self.ids.name.text = name
+       self.ids.address.text = address
+       self.ids.photo.source = photo
+
+# Classes com as telas
+class Resultados(Screen):
+    ...
+   
+class Inicial(Screen):
+    ...
+
+class TelaLogin(Screen):
+    ...
+
+class TelaHome(Screen):
+    def busca(self):
+        comida = self.ids.mealType.text
+        local = self.ids.location.text
+
+        resposta = finder.find(comida, local)
+        print(resposta)
+        return resposta 
+
+    def abrir_card(self,name,address,photo):
+        self.ids.box.add_widget(ResultadosCard(name=name, address=address, photo=photo))
+
+class TelaConta(Screen): 
+    ...
+
+class TelaFavoritos(Screen):
+    ...
+
+# Screen manager
+sm = ScreenManager()
+sm.add_widget(TelaLogin(name='tela_login'))
+sm.add_widget(TelaLogin(name='tela_home'))
+sm.add_widget(TelaLogin(name='tela_conta'))
+sm.add_widget(TelaLogin(name='tela_favoritos'))
+sm.add_widget(Inicial(name='inicial'))
+sm.add_widget(Resultados(name='resultados'))
+sm.add_widget(TelaHome(name='home'))
+
+
+# Construção principal do app
+class Myapp(MDApp):
+    def build(self):
+        #self.theme_cls.primary_palette = 'DeepOrange'
+        #self.theme_cls.accent_palette = 'Red'
+        return Builder.load_file('interface.kv')
+    
+# Inicia o aplicativo
+Myapp().run()
+
