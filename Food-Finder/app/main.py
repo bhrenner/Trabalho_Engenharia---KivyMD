05ec2af4d06a6b0628ba2db18f39fd9cde2027ff
Bhrenner Wilson <89161865+bhrenner@users.noreply.github.com>
2022-06-09 00:31:42 -0300

    Att do food

--- Food-Finder/app/main.py
+++ Food-Finder/app/main.py
@@ -2,18 +2,14 @@ from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivymd.uix.card import MDCard
-from kivy.core.window import Window
 from kivy.clock import Clock
+from kivy.core.window import Window
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDFlatButton,MDRectangleFlatButton
 import finder 
+import db_conn
 
-Window.size = (400,750)
-
-class Splash(Screen):
-    def login(self):
-        MDApp.get_running_app().root.current = "tela_login"
-    
-    Clock.schedule_once(login, 5)
-
+Window.size = (470,750)
 
 class ResultadosCard(MDCard):
    def __init__(self,name='',address='',photo='',**kwargs):
@@ -21,53 +17,80 @@ class ResultadosCard(MDCard):
        self.ids.name.text = name
        self.ids.address.text = address
        self.ids.photo.source = photo
+       
+class SenhaCard(MDCard):
+    def fechar(self):
+        self.parent.remove_widget(self)
 
-# Classes com as telas
-class Resultados(Screen):
-    ...
-   
-class Inicial(Screen):
-    ...
+class ExcluirCard(MDCard):
+    def fechar(self):
+        self.parent.remove_widget(self)
 
-class TelaLogin(Screen):
-    ...
+class DemoProject(ScreenManager):
+    
+    def login(self):
+        MDApp.get_running_app().root.current = "tela_login"
 
-class TelaHome(Screen):
+    Clock.schedule_once(login, 5)
+
     def busca(self):
         comida = self.ids.mealType.text
         local = self.ids.location.text
-
         resposta = finder.find(comida, local)
-        print(resposta)
         return resposta 
 
     def abrir_card(self,name,address,photo):
-        self.ids.box.add_widget(ResultadosCard(name=name, address=address, photo=photo))
+        self.ids.results_box.add_widget(ResultadosCard(name=name, address=address, photo=photo))
 
-class TelaConta(Screen): 
+    def remove_cards(self):
+        while True:
+            try:
+                self.ids.results_box.remove_widget(self.ids.results_box.children[0])
+            except:
+                break
+
+    def navigation(self, tela):
+        MDApp.get_running_app().root.current = tela
+
+    def cadastrar(self):
+        nome = self.ids.cdt_nome.text
+        cpf = self.ids.cdt_cpf.text
+        data_nasc = self.ids.cdt_data_nascimento.text
+        email = self.ids.cdt_email.text
+        senha = self.ids.cdt_senha.text
+
+        try:
+            retorno = db_conn.cadastrar(nome, cpf, data_nasc, email, senha)
+            if retorno == "Cadastro concluido com sucesso!":
+                self.dialog = MDDialog(title = 'Cadastro Concluído!', buttons=[MDFlatButton(text='Concluir')])
+                self.dialog.open()
+        except:
+            self.dialog = MDDialog(title = 'Erro',text = 'Tente novamente' )
+            self.dialog.open()
+
+  
+        
+
+class TelaHome(Screen):
     ...
 
-class TelaFavoritos(Screen):
+class TelaConta(Screen):
+    def alter_senha(self): #método para abrir o card
+        self.add_widget(SenhaCard())
+
+    def excluir_conta(self): #método para abrir o card
+        self.add_widget(ExcluirCard())
     ...
 
-# Screen manager
-sm = ScreenManager()
-sm.add_widget(TelaLogin(name='tela_login'))
-sm.add_widget(TelaLogin(name='tela_home'))
-sm.add_widget(TelaLogin(name='tela_conta'))
-sm.add_widget(TelaLogin(name='tela_favoritos'))
-sm.add_widget(Inicial(name='inicial'))
-sm.add_widget(Resultados(name='resultados'))
-sm.add_widget(TelaHome(name='home'))
+class ListaResultados(Screen):
+    ...
 
-
-# Construção principal do app
 class Myapp(MDApp):
     def build(self):
-        #self.theme_cls.primary_palette = 'DeepOrange'
-        #self.theme_cls.accent_palette = 'Red'
-        return Builder.load_file('interface.kv')
+        self.theme_cls.primary_palette = 'DeepOrange'
+        #self.theme_cls.primary_light = ""
+        Builder.load_file('interface.kv')
+        return DemoProject()
     
 # Inicia o aplicativo
 Myapp().run()
-
